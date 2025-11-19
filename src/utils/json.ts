aac17d4a529be925f7270d411ad55700398a58b3
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-11-18 21:21:26 -0300

    feat: implement stringify function with customizable options

---
+++ src/utils/json.ts
@@ -0,0 +1,38 @@
+interface StringifyOptions {
+  replacer?: (
+    this: unknown,
+    key: string,
+    value: unknown,
+  ) => unknown | (number | string)[] | null;
+  space?: string | number;
+  escapeHtml?: boolean;
+}
+
+export function stringify(
+  value: unknown,
+  options: StringifyOptions = {},
+): string {
+  const { replacer, space, escapeHtml } = options;
+
+  const json =
+    replacer || space
+      ? JSON.stringify(value, replacer, space)
+      : JSON.stringify(value);
+
+  if (escapeHtml && typeof json === 'string') {
+    return json.replace(/[<>&]/g, (c) => {
+      switch (c.charCodeAt(0)) {
+        case 0x3c: // <
+          return '\u003c';
+        case 0x3e: // >
+          return '\u003e';
+        case 0x26: // &
+          return '\u0026';
+        default:
+          return c;
+      }
+    });
+  }
+
+  return json;
+}
